01ae952ea425f05b4754a0f14d7739003f76f457
realAyinde <ayindesodeeq2@gmail.com>
2020-10-09 01:01:00 +0100

    make changes

--- src/components/about.js
+++ src/components/about.js
@@ -26,7 +26,7 @@ const about = `
           </div>
           <div class="col-12 col-lg-6">
             <div class="display-pic">
-              <img src="./assets/my_pic.jpg" id="my-pic" class="my-pic"/>
+
               <div class="pic-texts">
                 <p class="name">AYINDE Sodiq</p>
                 <p class="title">Freelance Software Engineer</p>
